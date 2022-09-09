62a9974dc480fa1c3c239b5b6a9e0bce8b549c3f
DeviousLab <deviouslab@gmail.com>
2022-09-09 16:20:49 +0400

    Created exported functions that call the API client
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- index.ts
+++ index.ts
@@ -1,14 +1,45 @@
-export function helloWorld() {
-  const message = 'Hello World from my example modern npm package!';
-  return message;
+import { Resources } from './src/types';
+import { ApiClient } from './src/client';
+
+const client = new ApiClient<Resources>({
+	endpoint: 'https://deezjokes.deta.dev',
+});
+
+async function getAllDNJokes(max_results: number = 48) {
+	const [status, data] = await client.find('jokes', { max_results });
+	if (status === 'failed') {
+		throw Error(`FAILED: ${data.message}`);
+	}
+	return data;
 }
 
-export function goodBye() {
-  const message = 'Goodbye from my example modern npm package!';
-  return message;
+async function getRandomDNJoke() {
+	const [status, data] = await client.find('joke/random');
+	if (status === 'failed') {
+		throw Error(`FAILED: ${data.message}`);
+	}
+	return data;
+}
+
+async function getDNJokeByQuery(keyword: string) {
+	const [status, data] = await client.find('joke/search', { keyword });
+	if (status === 'failed') {
+		throw Error(`FAILED: ${data.message}`);
+	}
+	return data;
+}
+
+async function getDNJokeById(dn_id: number = 1) {
+	const [status, data] = await client.find('joke/', { dn_id });
+	if (status === 'failed') {
+		throw Error(`FAILED: ${data.message}`);
+	}
+	return data;
 }
 
 export default {
-  helloWorld,
-  goodBye,
+	getAllDNJokes,
+	getRandomDNJoke,
+	getDNJokeByQuery,
+	getDNJokeById,
 };
