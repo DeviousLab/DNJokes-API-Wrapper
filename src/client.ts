8eabeea193a25ac3b92b31fe33a057f7fa4bae28
DeviousLab <deviouslab@gmail.com>
2022-09-09 12:07:35 +0400

    Installed axios and implemented axios client to fetch responses
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

---
+++ src/client.ts
@@ -0,0 +1,48 @@
+import axios, { AxiosError, AxiosRequestConfig } from 'axios';
+
+type ClientConfig = {
+  endpoint: `https://${string}`;
+}
+
+type Keys<T> = keyof T & string;
+
+type Params<T> = {
+  [K in keyof T]?: T[K]
+} & { max_results?: number | 49, keyword?: string};
+
+class ApiClient<TData> { 
+  private endpoint: string;
+
+  constructor(config: ClientConfig) {
+    this.endpoint = config.endpoint;
+  }
+
+  private async request<T>(url: URL, config: AxiosRequestConfig) {
+    try {
+      const res = await axios({
+        ...config,
+        url: url.toString(),
+      })
+      return ["success", res.data as T] as const;
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        console.error(`API_CLIENT_ERROR: ${error.message}`);
+      }
+      return ["failed", error as AxiosError] as const;
+    }
+  }
+
+  find<R extends Keys<TData>>(resource: R, params?: Params<TData[R]>) {
+    const search = new URLSearchParams()
+    if (params) {
+      for (let [key, value] of Object.entries(params)) {
+        search.set(key, value as string);
+      }
+    }
+    const url = new URL(`${resource}?${search}`, this.endpoint);
+    console.log(url.toString());
+    return this.request<TData[R][]>(url, { method: 'GET' });
+  }
+}
+
+export { ApiClient };
